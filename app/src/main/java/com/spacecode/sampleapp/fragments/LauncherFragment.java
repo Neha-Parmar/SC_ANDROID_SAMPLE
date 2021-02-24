c1d51b4c89cc8b30f78f4a11bfbfdfa197fadfcf
Neha Parmar <neha.parmar@spacecode.com>
2021-02-24 18:55:21 +0530

    Updated Mirrar sdk to master-v3 Updated minsdk version as per JT App

--- app/src/main/java/com/spacecode/sampleapp/fragments/LauncherFragment.java
+++ app/src/main/java/com/spacecode/sampleapp/fragments/LauncherFragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.sdm.mirrar.library.MirrarFragment;
+import com.sdm.mirrar.mirrar.MirrarFragment;
 import com.spacecode.sampleapp.R;
 
 import org.json.JSONException;
@@ -51,12 +51,12 @@ public class LauncherFragment extends BaseFragment {
             public void onClick(View view) {
                 try {
                     JSONObject data = new JSONObject("{\"options\":{\"productData\":{\"Earrings\":{\"items\":[\"500005DMAABA04\",\"20KE10\",\"20KE11\",\"500005SFAABA02\",\"500005SFAABA09\"],\"type\":\"ear\"},\"Mangalsutras\":{\"items\":[\"502118YEEAAA32\",\"502118YENAAA32\",\"502118YEOAAA32\"],\"type\":\"neck\"},\"NecklaceSets\":{\"items\":[\"5027182ADABA02\",\"5027182AJABA02\",\"5027182AKABA02\"],\"type\":\"set\"},\"Necklaces\":{\"items\":[\"502516NFEAAB32_N\",\"CH11\",\"CH12\",\"5027182ADABA02_N\",\"5027182AJABA02_N\"],\"type\":\"neck\"},\"PendantSets\":{\"items\":[\"5031181DXAAA09\",\"5132181BUABA00\",\"5132181BWABA00\"],\"type\":\"set\"},\"Pendants\":{\"items\":[\"500005PFAAAA09\",\"500005PFAAAB22\",\"500006PEAAAA09\"],\"type\":\"neck\"}}}}");
-                    fragment = new MirrarFragment(data, "jewel-trace", "2555");
+                    fragment = new MirrarFragment("jewel-trace", "2555", data);
 
-                    /*FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+                    FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
 
                     transaction.replace(R.id.container_layout, fragment).commit();
-                    btnLaunch.setVisibility(View.GONE);*/
+                    btnLaunch.setVisibility(View.GONE);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
